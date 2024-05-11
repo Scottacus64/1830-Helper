41335b85b7336a0f92a74d3d523c73ad1569f9db
Scottacus64 <70824106+Scottacus64@users.noreply.github.com>
2024-05-11 07:32:06 -0500

    switch to absolute window pos

--- station_pushbutton.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 11 06:45:54 2024
-
-@author: scottmiller
-"""
-
--- train_pushbutton.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-# -*- coding: utf-8 -*-
-"""
-Created on Sat May 11 06:47:43 2024
-
-@author: scottmiller
-"""
-
